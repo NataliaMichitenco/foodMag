3c4e1c8f9d540b34adb08597af686e47e7783f4d
Tusea <tuseamiki@gmail.com>
2024-11-12 10:59:29 +0200

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-
+  base: "/foodMag",
+  
   define: {
     'process.env': {} // Это заменяет использование process.env на пустой объект в браузере
   }
